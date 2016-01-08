6fd15a4b5493ba67d4aae928f525fe6c34db0bb3
William Boles <william.boles@unii.com>
2016-01-08 17:57:43 +0000

    Refactored CDSCount into a category on NSManagedObjectContext to ensure that the developers needs to consider what context they are using - think about threads

---
+++ CoreDataServices/Services/Count/NSManagedObjectContext+CDSCount.h
@@ -0,0 +1,35 @@
+//
+//  NSManagedObjectContext+CDSCount.h
+//  CoreDataServices
+//
+//  Created by William Boles on 08/01/2016.
+//  Copyright © 2016 Boles. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObjectContext (CDSCount)
+
+/*
+ Retrieves the count of entries from default context
+ 
+ @param entityClass - a class value for the entity in core data
+ 
+ @return count of entries for this class/entity
+ */
+- (NSUInteger)retrieveEntriesCountForEntityClass:(Class)entityClass;
+
+
+/*
+ Retrieves the count of entries that match the provided predicate's conditions from default context
+ 
+ @param entityClass - a class value for the entity in core data
+ @param predicate - a predicate used to limit the entries returned
+ 
+ @return count of entries that match provided predicate
+ */
+- (NSUInteger)retrieveEntriesCountForEntityClass:(Class)entityClass
+                                       predicate:(NSPredicate *)predicate;
+
+
+@end
