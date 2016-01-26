93ab831c6c198d5fd094073ad3ac0edf5be4408b
William Boles <william.boles@unii.com>
2016-01-26 14:17:28 +0000

    Updated comments on service manager

--- CoreDataServices/Manager/CDSServiceManager.h
+++ CoreDataServices/Manager/CDSServiceManager.h
@@ -10,12 +10,15 @@
 @import Foundation;
 @import UIKit;
 
+/**
+ A singleton manager that is responsible for setting up a typical (one context) core data stack and providing access to the main queue context. Also contains some convenience methods for interacting with that main queue context.
+ */
 @interface CDSServiceManager : NSObject
 
 /*
  `NSManagedObjectContext` instance that is used as the default context.
  
- This context should be used on the main thread. Setup using concurrancy type: `NSMainQueueConcurrencyType`.
+ This context should be used on the main thread - using concurrancy type: `NSMainQueueConcurrencyType`.
  
  @return `NSManagedObjectContext` instance.
  */
@@ -24,7 +27,7 @@
 /*
  `NSManagedObjectContext` instance that is used as the background context
  
- This context should be used on a background thread. Setup using concurrancy type: `NSPrivateQueueConcurrencyType`.
+ This context should be used on a background thread - using concurrancy type: `NSPrivateQueueConcurrencyType`.
  
  return `NSManagedObjectContext` instance.
  */
@@ -33,7 +36,7 @@
 /*
  Returns the global CDSServiceManager instance.
  
- @return CDSServiceManager instance.
+ @return CDSServiceManager shared instance.
  */
 + (instancetype)sharedInstance;
 
@@ -45,12 +48,12 @@
 - (void)setupModelURLWithModelName:(NSString *)name;
 
 /*
- Saves the mainManagedObjectContext.
+ Saves the managed object context that is used via the `mainManagedObjectContext` property.
  */
 - (void)saveMainManagedObjectContext;
 
 /*
- Saves the backgroundManagedObjectContext.
+ Saves the managed object context that is set used the `NSPrivateQueueConcurrencyType` property.
  
  Saving the backgroundManagedObjectContext will cause the mainManagedObjectContext to be saved. This can result in a slightly longer save operation however the trade-off is to ensure "data correctness" over performance.
  */
